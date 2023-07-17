148e90ca8a80d5a83e6af0f66373951983e64c90
Gary <cnahzjl@gmail.com>
2023-07-17 17:48:11 +0800

    初始化仓库

---
+++ qq-channel-bot-core/src/main/java/cn/zjiali/bot/core/BotConfiguration.java
@@ -0,0 +1,40 @@
+package cn.zjiali.bot.core;
+
+/**
+ * @author zJiaLi
+ * @since 2023-07-17 11:26
+ */
+public class BotConfiguration {
+
+    private String appId;
+
+    private String token;
+
+    public String authorization() {
+        return "Bot " + this.appId + "." + this.token;
+    }
+
+    public BotConfiguration() {
+    }
+
+    public BotConfiguration(String appId, String token) {
+        this.appId = appId;
+        this.token = token;
+    }
+
+    public String getAppId() {
+        return appId;
+    }
+
+    public void setAppId(String appId) {
+        this.appId = appId;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}

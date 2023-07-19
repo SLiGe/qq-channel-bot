42e5d6b7ecd15df557cec205b4c62063205525af
Gary <cnahzjl@gmail.com>
2023-07-19 14:27:39 +0800

    添加事件对象

--- qq-channel-bot-core/src/main/java/cn/zjiali/bot/core/event/model/MessageEvent.java
+++ qq-channel-bot-core/src/main/java/cn/zjiali/bot/core/event/model/MessageEvent.java
@@ -1,10 +1,10 @@
 package cn.zjiali.bot.core.event.model;
 
-import cn.zjiali.bot.core.model.Member;
+import cn.zjiali.bot.core.model.message.Message;
 
 /**
  * @author zJiaLi
  * @since 2023-07-19 11:03
  */
-public class MessageEvent extends Member {
+public class MessageEvent extends Message {
 }
